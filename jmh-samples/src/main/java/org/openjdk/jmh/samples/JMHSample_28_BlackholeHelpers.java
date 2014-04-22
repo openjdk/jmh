8f990b90c2a36e23df2e7512561991fc5b1cebf3
Aleksey Shipilev <shade@openjdk.org>
2014-04-23 00:28:17 +0400

    JMHSample_28_BlackholeHelpers: showcase Blackholes injectable in @Setup/@TearDown

---
+++ jmh-samples/src/main/java/org/openjdk/jmh/samples/JMHSample_28_BlackholeHelpers.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jmh.samples;
+
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.logic.BlackHole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@State(Scope.Thread)
+public class JMHSample_28_BlackholeHelpers {
+
+    /**
+     * Sometimes you need the black hole not in @GMB method, but in
+     * helper methods, because you want to pass it through to the concrete
+     * implementation which is instantiated in helper methods. In this case,
+     * you can request the black hole straight in the helper method signature.
+     * This applies to both @Setup and @TearDown methods, and also to other
+     * JMH infrastructure objects, like Control.
+     *
+     * Below is the variant of {@link org.openjdk.jmh.samples.JMHSample_08_DeadCode}
+     * test, but wrapped in the anonymous classes.
+     */
+
+    public interface Worker {
+        void work();
+    }
+
+    private Worker workerBaseline;
+    private Worker workerRight;
+    private Worker workerWrong;
+
+    @Setup
+    public void setup(final BlackHole bh) {
+        workerBaseline = new Worker() {
+            double x;
+
+            @Override
+            public void work() {
+                // do nothing
+            }
+        };
+
+        workerWrong = new Worker() {
+            double x;
+
+            @Override
+            public void work() {
+                Math.log(x);
+            }
+        };
+
+        workerRight = new Worker() {
+            double x;
+
+            @Override
+            public void work() {
+                bh.consume(Math.log(x));
+            }
+        };
+
+    }
+
+    @GenerateMicroBenchmark
+    public void baseline() {
+        workerBaseline.work();
+    }
+
+    @GenerateMicroBenchmark
+    public void measureWrong() {
+        workerWrong.work();
+    }
+
+    @GenerateMicroBenchmark
+    public void measureRight() {
+        workerRight.work();
+    }
+
+    /*
+     * ============================== HOW TO RUN THIS TEST: ====================================
+     *
+     * You will see measureWrong() running on-par with baseline().
+     * Both measureRight() are measuring twice the baseline, so the logs are intact.
+     *
+     * You can run this test:
+     *
+     * a) Via the command line:
+     *    $ mvn clean install
+     *    $ java -jar target/microbenchmarks.jar ".*JMHSample_28.*"
+     *
+     * b) Via the Java API:
+     */
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(".*" + JMHSample_28_BlackholeHelpers.class.getSimpleName() + ".*")
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
