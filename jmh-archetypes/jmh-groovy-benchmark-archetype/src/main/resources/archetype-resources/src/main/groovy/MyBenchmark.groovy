9aca8d5d4b879d540938f6940d75a4af96a98939
Aleksey Shipilev <shade@openjdk.org>
2014-05-08 18:30:44 +0400

    Jmh-groovy-benchmark-archetype: remove @State(Benchmark) from the default Groovy benchmark

--- jmh-archetypes/jmh-groovy-benchmark-archetype/src/main/resources/archetype-resources/src/main/groovy/MyBenchmark.groovy
+++ jmh-archetypes/jmh-groovy-benchmark-archetype/src/main/resources/archetype-resources/src/main/groovy/MyBenchmark.groovy
@@ -1,10 +1,7 @@
 package ${groupId};
 
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
 
-@State(Scope.Benchmark)
 class MyBenchmark {
 
     @GenerateMicroBenchmark
