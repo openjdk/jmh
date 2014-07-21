ac486a3c323ec66d5094e1792459ae5ab69f51bc
Aleksey Shipilev <shade@openjdk.org>
2014-07-21 22:14:46 +0400

    Add two missing tests

---
+++ jmh-core-it/src/test/java/org/openjdk/jmh/it/params/EnumBenchParamImplicitSequenceTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2005, 2014, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jmh.it.params;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.it.Fixtures;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@Measurement(iterations = 1, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Warmup(iterations = 1, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(1)
+@State(Scope.Thread)
+public class EnumBenchParamImplicitSequenceTest {
+
+    @Param({"VALUE_A", "VALUE_B", "VALUE_C"})
+    public SampleEnumA a;
+
+    @Param
+    public SampleEnumB b;
+
+    @Benchmark
+    public void test() {
+        Fixtures.work();
+    }
+
+    @Test
+    public void full() throws RunnerException {
+        Options opts = new OptionsBuilder()
+                .include(Fixtures.getTestMask(this.getClass()))
+                .shouldFailOnError(true)
+                .build();
+
+        Assert.assertEquals(3 * 3, new Runner(opts).run().size());
+    }
+
+    @Test
+    public void constrainedA() throws RunnerException {
+        Options opts = new OptionsBuilder()
+                .include(Fixtures.getTestMask(this.getClass()))
+                .shouldFailOnError(true)
+                .param("a", SampleEnumA.VALUE_A.name())
+                .build();
+
+        Assert.assertEquals(1 * 3, new Runner(opts).run().size());
+    }
+
+    @Test
+    public void constrainedB() throws RunnerException {
+        Options opts = new OptionsBuilder()
+                .include(Fixtures.getTestMask(this.getClass()))
+                .shouldFailOnError(true)
+                .param("b", SampleEnumB.VALUE_A.name())
+                .build();
+
+        Assert.assertEquals(1*3, new Runner(opts).run().size());
+    }
+
+    public static enum SampleEnumA {
+        VALUE_A, VALUE_B, VALUE_C;
+    }
+
+    public static enum SampleEnumB {
+        VALUE_A, VALUE_B, VALUE_C;
+    }
+}
