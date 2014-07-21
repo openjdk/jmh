c99ce8a57232fc1e4515b736a4eacfa8caeb6de1
Aleksey Shipilev <shade@openjdk.org>
2014-07-21 22:25:34 +0400

    Jmh-core-it: test non-referenced @Params are not used in configuration

---
+++ jmh-core-it/src/test/java/org/openjdk/jmh/it/params/IsolatedParamSequenceTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2005, 2014, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jmh.it.params;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.it.Fixtures;
+import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
+@Measurement(iterations = 1, time = 100, timeUnit = TimeUnit.MICROSECONDS)
+@Warmup(iterations = 1, time = 100, timeUnit = TimeUnit.MICROSECONDS)
+@Fork(1)
+public class IsolatedParamSequenceTest {
+
+    @State(Scope.Benchmark)
+    public static class MyState {
+        @Param({"1", "2", "3"})
+        public int x;
+    }
+
+    @Benchmark
+    public void test1(MyState s) {
+        Fixtures.work();
+    }
+
+    @Benchmark
+    public void test2() {
+        Fixtures.work();
+    }
+
+    @Test
+    public void onlyTest1() throws RunnerException {
+        Options opts = new OptionsBuilder()
+                .include(Fixtures.getTestMask(this.getClass()) + ".*test1")
+                .shouldFailOnError(true)
+                .build();
+
+        Collection<RunResult> run = new Runner(opts).run();
+        Assert.assertEquals(3, run.size());
+    }
+
+    @Test
+    public void onlyTest2() throws RunnerException {
+        Options opts = new OptionsBuilder()
+                .include(Fixtures.getTestMask(this.getClass()) + ".*test2")
+                .shouldFailOnError(true)
+                .build();
+
+        Collection<RunResult> run = new Runner(opts).run();
+        Assert.assertEquals(1, run.size());
+    }
+
+}
