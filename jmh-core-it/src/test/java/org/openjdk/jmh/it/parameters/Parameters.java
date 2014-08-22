f36c9a570601ef4f30629e94531e71b686d07045
Aleksey Shipilev <shade@openjdk.org>
2014-08-22 13:25:24 +0400

    Jmh-core-it: fix build failure in Parameters

--- jmh-core-it/src/test/java/org/openjdk/jmh/it/parameters/Parameters.java
+++ jmh-core-it/src/test/java/org/openjdk/jmh/it/parameters/Parameters.java
@@ -30,6 +30,7 @@ import org.openjdk.jmh.runner.BenchmarkListEntry;
 import org.openjdk.jmh.runner.format.OutputFormatFactory;
 import org.openjdk.jmh.runner.options.VerboseMode;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 
@@ -37,7 +38,7 @@ public class Parameters {
 
     public static BenchmarkListEntry get(Class<?> klass) {
         BenchmarkList list = BenchmarkList.fromFile("target/test-classes" + BenchmarkList.BENCHMARK_LIST);
-        Set<BenchmarkListEntry> set = list.find(OutputFormatFactory.createFormatInstance(System.out, VerboseMode.EXTRA), ".*" + klass.getName().replaceAll("\\$",".") + ".*", Collections.<String>emptyList());
+        Set<BenchmarkListEntry> set = list.find(OutputFormatFactory.createFormatInstance(System.out, VerboseMode.EXTRA), Arrays.asList(klass.getName().replaceAll("\\$",".")), Collections.<String>emptyList());
         Assert.assertEquals("The single benchmark exists", 1, set.size());
         return set.iterator().next();
     }
