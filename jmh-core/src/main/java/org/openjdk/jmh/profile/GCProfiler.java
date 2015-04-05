d8243f202434f06b3826205975471c65f22b0634
Aleksey Shipilev <shade@openjdk.org>
2015-04-05 14:13:57 +0300

    7901370: JMH -prof gc should report churn rate

--- jmh-core/src/main/java/org/openjdk/jmh/profile/GCProfiler.java
+++ jmh-core/src/main/java/org/openjdk/jmh/profile/GCProfiler.java
@@ -24,21 +24,62 @@
  */
 package org.openjdk.jmh.profile;
 
+import com.sun.management.GarbageCollectionNotificationInfo;
 import org.openjdk.jmh.infra.BenchmarkParams;
 import org.openjdk.jmh.infra.IterationParams;
 import org.openjdk.jmh.results.AggregationPolicy;
 import org.openjdk.jmh.results.IterationResult;
 import org.openjdk.jmh.results.Result;
+import org.openjdk.jmh.util.HashMultiset;
+import org.openjdk.jmh.util.Multiset;
 
+import javax.management.ListenerNotFoundException;
+import javax.management.Notification;
+import javax.management.NotificationEmitter;
+import javax.management.NotificationListener;
+import javax.management.openmbean.CompositeData;
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
-import java.util.Arrays;
+import java.lang.management.MemoryUsage;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class GCProfiler implements InternalProfiler {
+    private long beforeTime;
     private long beforeGCCount;
     private long beforeGCTime;
+    private final NotificationListener listener;
+    private Set<String> observedSpaces = new HashSet<String>();
+    private Multiset<String> churn = new HashMultiset<String>();
+
+    public GCProfiler() {
+        listener = new NotificationListener() {
+            @Override
+            public void handleNotification(Notification n, Object o) {
+                if (n.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) n.getUserData());
+
+                    Map<String, MemoryUsage> mapBefore = info.getGcInfo().getMemoryUsageBeforeGc();
+                    Map<String, MemoryUsage> mapAfter = info.getGcInfo().getMemoryUsageAfterGc();
+                    for (Map.Entry<String, MemoryUsage> entry : mapAfter.entrySet()) {
+                        String name = entry.getKey();
+                        MemoryUsage after = entry.getValue();
+                        MemoryUsage before = mapBefore.get(name);
+                        long c = before.getUsed() - after.getUsed();
+                        if (c > 0) {
+                            churn.add(name, c);
+                            observedSpaces.add(name);
+                        }
+                    }
+                }
+            }
+        };
+    }
 
     @Override
     public String getDescription() {
@@ -57,6 +98,8 @@ public class GCProfiler implements InternalProfiler {
 
     @Override
     public void beforeIteration(BenchmarkParams benchmarkParams, IterationParams iterationParams) {
+        installHooks();
+
         long gcTime = 0;
         long gcCount = 0;
         for (GarbageCollectorMXBean bean : ManagementFactory.getGarbageCollectorMXBeans()) {
@@ -65,10 +108,14 @@ public class GCProfiler implements InternalProfiler {
         }
         this.beforeGCCount = gcCount;
         this.beforeGCTime = gcTime;
+        this.beforeTime = System.nanoTime();
     }
 
     @Override
-    public Collection<? extends Result> afterIteration(BenchmarkParams benchmarkParams, IterationParams iterationParams, IterationResult result) {
+    public Collection<? extends Result> afterIteration(BenchmarkParams benchmarkParams, IterationParams iterationParams, IterationResult iResult) {
+        uninstallHooks();
+        long afterTime = System.nanoTime();
+
         long gcTime = 0;
         long gcCount = 0;
         for (GarbageCollectorMXBean bean : ManagementFactory.getGarbageCollectorMXBeans()) {
@@ -76,12 +123,59 @@ public class GCProfiler implements InternalProfiler {
             gcTime += bean.getCollectionTime();
         }
 
-        return Arrays.asList(
-                new ProfilerResult(Defaults.PREFIX + "gc.count.profiled", gcCount - beforeGCCount, "counts", AggregationPolicy.SUM),
-                new ProfilerResult(Defaults.PREFIX + "gc.count.total", gcCount, "counts", AggregationPolicy.MAX),
-                new ProfilerResult(Defaults.PREFIX + "gc.time.profiled", gcTime - beforeGCTime, "ms", AggregationPolicy.SUM),
-                new ProfilerResult(Defaults.PREFIX + "gc.time.total", gcTime, "ms", AggregationPolicy.MAX)
-        );
+        List<ProfilerResult> results = new ArrayList<ProfilerResult>();
+
+        results.add(new ProfilerResult(
+                Defaults.PREFIX + "gc.count",
+                gcCount - beforeGCCount,
+                "counts",
+                AggregationPolicy.SUM));
+
+        results.add(new ProfilerResult(
+                Defaults.PREFIX + "gc.time",
+                gcTime - beforeGCTime,
+                "ms",
+                AggregationPolicy.SUM));
+
+        for (String space : observedSpaces) {
+            double churnRate = 1.0 * churn.count(space) * TimeUnit.SECONDS.toNanos(1) / (afterTime - beforeTime);
+
+            results.add(new ProfilerResult(
+                    Defaults.PREFIX + "gc.churn.{" + space + "}",
+                    churnRate / 1024 / 1024,
+                    "MB/sec",
+                    AggregationPolicy.AVG));
+
+            results.add(new ProfilerResult(Defaults.PREFIX + "gc.churn.{" + space + "}.norm",
+                    churnRate / iResult.getMetadata().getAllOps(),
+                    "B/op",
+                    AggregationPolicy.AVG));
+        }
+
+        return results;
+    }
+
+    private boolean hooksInstalled;
+
+    public synchronized void installHooks() {
+        if (hooksInstalled) return;
+        hooksInstalled = true;
+        churn = new HashMultiset<String>();
+        for (GarbageCollectorMXBean bean : ManagementFactory.getGarbageCollectorMXBeans()) {
+            ((NotificationEmitter) bean).addNotificationListener(listener, null, null);
+        }
+    }
+
+    public synchronized void uninstallHooks() {
+        if (!hooksInstalled) return;
+        hooksInstalled = false;
+        for (GarbageCollectorMXBean bean : ManagementFactory.getGarbageCollectorMXBeans()) {
+            try {
+                ((NotificationEmitter) bean).removeNotificationListener(listener);
+            } catch (ListenerNotFoundException e) {
+                // Do nothing
+            }
+        }
     }
 
 }
