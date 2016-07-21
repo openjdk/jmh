748f3b220d783f55c4fd30cbbf3e056cc840a8f8
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-07-21 17:02:51 +0300

    7901738: Update Kotlin benchmark archetype

--- jmh-archetypes/jmh-kotlin-benchmark-archetype/src/main/resources/archetype-resources/src/main/kotlin/MyBenchmark.kt
+++ jmh-archetypes/jmh-kotlin-benchmark-archetype/src/main/resources/archetype-resources/src/main/kotlin/MyBenchmark.kt
@@ -35,7 +35,7 @@ import org.openjdk.jmh.annotations.Benchmark
 
 open class MyBenchmark {
 
-    [Benchmark]
+    @Benchmark
     fun testMethod() {
         // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
         // Put your benchmark code here.
