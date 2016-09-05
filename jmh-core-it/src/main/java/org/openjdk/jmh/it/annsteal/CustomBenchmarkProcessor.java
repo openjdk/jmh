a2faed68da8a0c83d5ba79f3357f11eaaad0dac5
Aleksey Shipilev <shade@openjdk.org>
2016-09-05 10:37:29 +0300

    7901779: JMH APT processor claims all annotations
    Should only capture JMH annotations.

--- jmh-core-it/src/main/java/org/openjdk/jmh/it/annsteal/CustomBenchmarkProcessor.java
+++ jmh-core-it/src/main/java/org/openjdk/jmh/it/annsteal/CustomBenchmarkProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,26 +22,24 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package org.openjdk.jmh.generators;
-
-import org.openjdk.jmh.generators.annotations.APGeneratorDestinaton;
-import org.openjdk.jmh.generators.annotations.APGeneratorSource;
-import org.openjdk.jmh.generators.core.BenchmarkGenerator;
-import org.openjdk.jmh.generators.core.GeneratorDestination;
-import org.openjdk.jmh.generators.core.GeneratorSource;
+package org.openjdk.jmh.it.annsteal;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Set;
 
-@SupportedAnnotationTypes("org.openjdk.jmh.annotations.*")
-public class BenchmarkProcessor extends AbstractProcessor {
+@SupportedAnnotationTypes("*")
+public class CustomBenchmarkProcessor extends AbstractProcessor {
 
-    private final BenchmarkGenerator generator = new BenchmarkGenerator();
+    public static final String CBP_LIST = "tmp/CBP.list";
 
     @Override
     public SourceVersion getSupportedSourceVersion() {
@@ -52,14 +50,22 @@ public class BenchmarkProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        GeneratorSource source = new APGeneratorSource(roundEnv, processingEnv);
-        GeneratorDestination destination = new APGeneratorDestinaton(roundEnv, processingEnv);
-        if (!roundEnv.processingOver()) {
-            generator.generate(source, destination);
-        } else {
-            generator.complete(source, destination);
+        for (TypeElement te : annotations) {
+            if (te.getQualifiedName().toString().equals(CustomAnnotation.class.getCanonicalName())) {
+                try {
+                    FileObject file = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT, "", CBP_LIST);
+                    Writer writer = file.openWriter();
+                    Set<? extends Element> els = roundEnv.getElementsAnnotatedWith(te);
+                    for (Element el : els) {
+                        writer.append(el.getSimpleName());
+                    }
+                    writer.close();
+                } catch (IOException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
         }
-        return true;
+        return false;
     }
 
 }
---
+++ jmh-core-it/src/test/java/org/openjdk/jmh/it/annsteal/CustomTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jmh.it.annsteal;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.util.FileUtils;
+import org.openjdk.jmh.util.Utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+
+@CustomAnnotation // here!
+public class CustomTest {
+
+    @Benchmark
+    public void bench() {
+        // intentionally left blank
+    }
+
+    @Test
+    public void test() throws IOException {
+        File file = FileUtils.extractFromResource('/' + CustomBenchmarkProcessor.CBP_LIST);
+        Collection<String> strings = FileUtils.readAllLines(file);
+        Assert.assertTrue(strings.contains(CustomTest.class.getSimpleName()));
+    }
+
+}
