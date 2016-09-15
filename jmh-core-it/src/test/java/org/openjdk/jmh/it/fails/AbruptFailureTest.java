2a6ec77aace2e012a5082ebe3c8adbc15080b6cd
Aleksey Shipilev <shade@openjdk.org>
2016-09-15 11:53:02 +0200

    Add missing test for 7901789

---
+++ jmh-core-it/src/test/java/org/openjdk/jmh/it/fails/AbruptFailureTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jmh.it.fails;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.ThreadParams;
+import org.openjdk.jmh.it.Fixtures;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Baseline test:
+ * Checks if assertions are propagated back to integration tests.
+ */
+@BenchmarkMode(Mode.All)
+@Warmup(iterations = 0)
+@Measurement(iterations = 1, time = 60, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@State(Scope.Benchmark)
+public class AbruptFailureTest {
+
+    @Benchmark
+    @Threads(4)
+    public void test(ThreadParams tp) {
+        if (tp.getThreadIndex() == 0) {
+            throw new IllegalStateException(); // fail one thread
+        }
+        Fixtures.work();
+    }
+
+    @Test
+    public void invokeAPI() throws RunnerException {
+        long time1 = System.nanoTime();
+        try {
+            Options opt = new OptionsBuilder()
+                    .include(Fixtures.getTestMask(this.getClass()))
+                    .shouldFailOnError(true)
+                    .build();
+            new Runner(opt).run();
+
+            Assert.fail("Should have failed");
+        } catch (Throwable t) {
+            // expected
+            long time2 = System.nanoTime();
+            long delay = TimeUnit.NANOSECONDS.toSeconds(time2 - time1);
+            Assert.assertTrue("Delayed for too long: " + delay + "s", delay < 10);
+        }
+    }
+
+}
