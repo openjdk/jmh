6e468d0e3ccf76a01514cd632b7038dea1392f8c
Sergey Kuksenko <skuksenko@gmail.com>
2023-09-07 20:39:00 -0700

    CODETOOLS-7903554: JMH: add Threads.HALF_MAX constant

---
+++ jmh-core-it/src/test/java/org/openjdk/jmh/it/threads/HalfMaxThreadCountTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jmh.it.threads;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.it.Fixtures;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.All)
+@State(Scope.Benchmark)
+public class HalfMaxThreadCountTest {
+
+    private final Set<Thread> threads = Collections.synchronizedSet(new HashSet<>());
+
+    @TearDown(Level.Iteration)
+    public void tearDown() {
+        if (Fixtures.expectStableThreads()) {
+            Assert.assertEquals(threads.size(), Runtime.getRuntime().availableProcessors() / 2);
+        } else {
+            Assert.assertTrue(threads.size() >= Runtime.getRuntime().availableProcessors() / 2);
+        }
+    }
+
+    @Benchmark
+    @Measurement(iterations = 1, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+    @Warmup(iterations = 0)
+    @Fork(1)
+    @Threads(Threads.HALF_MAX)
+    public void test1() {
+        threads.add(Thread.currentThread());
+        Fixtures.work();
+    }
+
+    @Benchmark
+    @Measurement(iterations = 1, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+    @Warmup(iterations = 0)
+    @Fork(1)
+    public void test2() {
+        threads.add(Thread.currentThread());
+        Fixtures.work();
+    }
+
+    @Test
+    public void invokeAPI_1() throws RunnerException {
+        for (int c = 0; c < Fixtures.repetitionCount(); c++) {
+            Options opt = new OptionsBuilder()
+                    .include(Fixtures.getTestMask(this.getClass()) + ".*test1")
+                    .shouldFailOnError(true)
+                    .build();
+            new Runner(opt).run();
+        }
+    }
+
+    @Test
+    public void invokeAPI_2() throws RunnerException {
+        for (int c = 0; c < Fixtures.repetitionCount(); c++) {
+            Options opt = new OptionsBuilder()
+                    .include(Fixtures.getTestMask(this.getClass()) + ".*test2")
+                    .shouldFailOnError(true)
+                    .threads(Threads.HALF_MAX)
+                    .build();
+            new Runner(opt).run();
+        }
+    }
+}
