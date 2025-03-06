eba7173d327da5b563c1a1d102c1c0a088a36664
Volkan Yazıcı <volkan@yazi.ci>
2025-03-06 20:25:48 +0100

    Fix copyright years

--- jmh-samples/src/main/java/org/openjdk/jmh/samples/JMHSample_38_PerInvokeSetup.java
+++ jmh-samples/src/main/java/org/openjdk/jmh/samples/JMHSample_38_PerInvokeSetup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Oracle America, Inc.
+ * Copyright (c) 2015, 2025, Oracle America, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
