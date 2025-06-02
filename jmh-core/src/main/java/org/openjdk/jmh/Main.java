a9a8184f2e6f13cab30921c8e7ad12f1591144d2
Galder Zamarreño <galder@zamarreno.com>
2025-06-02 18:41:43 +0200

    Enable alternative Runner instantiation and Main reuse

--- jmh-core/src/main/java/org/openjdk/jmh/Main.java
+++ jmh-core/src/main/java/org/openjdk/jmh/Main.java
@@ -37,67 +37,78 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] argv) throws IOException {
+        Main main = new Main();
+
+        CommandLineOptions cmdOptions = main.createCommandLineOptions(argv);
+
+        Runner runner = new Runner(cmdOptions);
+
+        main.run(cmdOptions, runner);
+    }
+
+    protected CommandLineOptions createCommandLineOptions(String[] argv)
+    {
         try {
-            CommandLineOptions cmdOptions = new CommandLineOptions(argv);
-
-            Runner runner = new Runner(cmdOptions);
-
-            if (cmdOptions.shouldHelp()) {
-                cmdOptions.showHelp();
-                return;
-            }
-
-            if (cmdOptions.shouldList()) {
-                runner.list();
-                return;
-            }
-
-            if (cmdOptions.shouldListWithParams()) {
-                runner.listWithParams(cmdOptions);
-                return;
-            }
-
-            if (cmdOptions.shouldListProfilers()) {
-                cmdOptions.listProfilers();
-                return;
-            }
-
-            if (cmdOptions.shouldListResultFormats()) {
-                cmdOptions.listResultFormats();
-                return;
-            }
-
-            try {
-                runner.run();
-            } catch (NoBenchmarksException e) {
-                System.err.println("No matching benchmarks. Miss-spelled regexp?");
-
-                if (cmdOptions.verbosity().orElse(Defaults.VERBOSITY) != VerboseMode.EXTRA) {
-                    System.err.println("Use " + VerboseMode.EXTRA + " verbose mode to debug the pattern matching.");
-                } else {
-                    runner.list();
-                }
-                System.exit(1);
-            } catch (ProfilersFailedException e) {
-                // This is not exactly an error, print all messages and set the non-zero exit code.
-                Throwable ex = e;
-                while (ex != null) {
-                    System.err.println(ex.getMessage());
-                    for (Throwable supp : ex.getSuppressed()) {
-                        System.err.println(supp.getMessage());
-                    }
-                    ex = ex.getCause();
-                }
-                System.exit(1);
-            } catch (RunnerException e) {
-                System.err.print("ERROR: ");
-                e.printStackTrace(System.err);
-                System.exit(1);
-            }
-
+            return new CommandLineOptions(argv);
         } catch (CommandLineOptionException e) {
             System.err.println("Error parsing command line:");
             System.err.println(" " + e.getMessage());
+            System.exit(1);
+            return null;
+        }
+    }
+
+    protected void run(CommandLineOptions cmdOptions, Runner runner) throws IOException {
+        if (cmdOptions.shouldHelp()) {
+            cmdOptions.showHelp();
+            return;
+        }
+
+        if (cmdOptions.shouldList()) {
+            runner.list();
+            return;
+        }
+
+        if (cmdOptions.shouldListWithParams()) {
+            runner.listWithParams(cmdOptions);
+            return;
+        }
+
+        if (cmdOptions.shouldListProfilers()) {
+            cmdOptions.listProfilers();
+            return;
+        }
+
+        if (cmdOptions.shouldListResultFormats()) {
+            cmdOptions.listResultFormats();
+            return;
+        }
+
+        try {
+            runner.run();
+        } catch (NoBenchmarksException e) {
+            System.err.println("No matching benchmarks. Miss-spelled regexp?");
+
+            if (cmdOptions.verbosity().orElse(Defaults.VERBOSITY) != VerboseMode.EXTRA) {
+                System.err.println("Use " + VerboseMode.EXTRA + " verbose mode to debug the pattern matching.");
+            } else {
+                runner.list();
+            }
+            System.exit(1);
+        } catch (ProfilersFailedException e) {
+            // This is not exactly an error, print all messages and set the non-zero exit code.
+            Throwable ex = e;
+            while (ex != null) {
+                System.err.println(ex.getMessage());
+                for (Throwable supp : ex.getSuppressed()) {
+                    System.err.println(supp.getMessage());
+                }
+                ex = ex.getCause();
+            }
+            System.exit(1);
+        } catch (RunnerException e) {
+            System.err.print("ERROR: ");
+            e.printStackTrace(System.err);
             System.exit(1);
         }
     }
